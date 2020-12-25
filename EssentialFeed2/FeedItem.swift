a3559e85260254c858c16e36c4ce83e876d47fdf
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-12-25 17:58:04 -0500

    Create the FeedItem module

---
+++ EssentialFeed2/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed2
+//
+//  Created by Ricardo Herrera Petit on 12/25/20.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id:UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL 
+}
