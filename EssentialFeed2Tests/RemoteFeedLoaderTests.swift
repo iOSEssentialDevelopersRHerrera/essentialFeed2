b2246a073abfa29693ef80375e0f3ea26050e03d
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-12-26 17:24:41 -0500

    Add remote feed loader result type with success and failure cases

--- EssentialFeed2Tests/RemoteFeedLoaderTests.swift
+++ EssentialFeed2Tests/RemoteFeedLoaderTests.swift
@@ -54,8 +54,6 @@ class RemoteFeedLoaderTests: XCTestCase {
                 client.complete(withStatusCode: code, at: index)
                 
             })
-            
-           
         }
     }
     
@@ -70,6 +68,12 @@ class RemoteFeedLoaderTests: XCTestCase {
         
     }
     
+//    func test_load_deliversNoItemsOn200HTTPRepsonseWithEmptyJSONList() {
+//        let (sut, client) = makeSUT()
+//
+//        var capture
+//    }
+    
     //MARK: - Helpers
     
     private func makeSUT(url:URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client:HTTPClientSpy) {
@@ -80,13 +84,13 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     private func expect(_ sut:RemoteFeedLoader, toCompleteWithError error:RemoteFeedLoader.Error, when action: ()-> Void, file: StaticString = #file, line:UInt = #line) {
        
-        var capturedErrors = [RemoteFeedLoader.Error]()
+        var capturedResults = [RemoteFeedLoader.Result]()
         
-        sut.load { capturedErrors.append($0) }
+        sut.load { capturedResults.append($0) }
         
         action()
         
-        XCTAssertEqual(capturedErrors, [error], file: file, line: line)
+        XCTAssertEqual(capturedResults, [.failure(error)], file: file, line: line)
     }
     
     private class HTTPClientSpy:HTTPClient {
