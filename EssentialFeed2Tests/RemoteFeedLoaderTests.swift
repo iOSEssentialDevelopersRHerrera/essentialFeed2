c315fc6e9346e13082036bfaf43039a2cf44a57c
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-12-25 22:35:18 -0500

    Changed to look as Canos lecture

--- EssentialFeed2Tests/RemoteFeedLoaderTests.swift
+++ EssentialFeed2Tests/RemoteFeedLoaderTests.swift
@@ -50,8 +50,8 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversErrorOnNon200HTTPresponse() {
         let(sut, client) = makeSUT()
-        let samples = [199,201, 300, 400, 500].enumerated()
-        samples.forEach { index, code in
+        let samples = [199,201, 300, 400, 500]
+        samples.enumerated().forEach { index, code in
             var capturedErrors = [RemoteFeedLoader.Error]()
             
             sut.load { capturedErrors.append($0) }
