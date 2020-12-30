cf2b75d90376a907fa9136b859f1a43fb400175c
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-12-29 19:57:17 -0500

    Need to override the correct function. Thats why it was not working before.

--- EssentialFeed2Tests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed2Tests/Feed API/URLSessionHTTPClientTests.swift
@@ -1,26 +1,24 @@
 //
-//  URLSessionHTTPClientTests.swift
-//  EssentialFeed2Tests
-//
-//  Created by Ricardo Herrera Petit on 12/29/20.
+//  Copyright © 2019 Essential Developer. All rights reserved.
 //
 
 import XCTest
 
 class URLSessionHTTPClient {
-    private let session:URLSession
+    private let session: URLSession
     
-    init(session:URLSession) {
+    init(session: URLSession) {
         self.session = session
     }
     
-    func get(from url:URL) {
-        session.dataTask(with: url)
+    func get(from url: URL) {
+        session.dataTask(with: url) { _, _, _ in }
     }
 }
 
 class URLSessionHTTPClientTests: XCTestCase {
-    func test_getFromURL_createDataTaskWithURL() {
+    
+    func test_getFromURL_createsDataTaskWithURL() {
         let url = URL(string: "http://any-url.com")!
         let session = URLSessionSpy()
         let sut = URLSessionHTTPClient(session: session)
@@ -30,15 +28,17 @@ class URLSessionHTTPClientTests: XCTestCase {
         XCTAssertEqual(session.receivedURLs, [url])
     }
     
-    //MARK: - Helpers 
-    private class URLSessionSpy:URLSession {
+    // MARK: - Helpers
+    
+    private class URLSessionSpy: URLSession {
         var receivedURLs = [URL]()
         
-        override func dataTask(with url: URL) -> URLSessionDataTask {
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
             receivedURLs.append(url)
             return FakeURLSessionDataTask()
         }
     }
     
     private class FakeURLSessionDataTask: URLSessionDataTask {}
+    
 }
