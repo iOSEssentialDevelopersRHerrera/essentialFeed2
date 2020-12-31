e814153117ca9d1e454f3b6862fbfc843ef8ec9e
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-12-31 12:30:40 -0500

    Save command requests new cache insertion on successful deletion

--- EssentialFeed2Tests/Feed Cache/CacheFeedUseCase.swift
+++ EssentialFeed2Tests/Feed Cache/CacheFeedUseCase.swift
@@ -16,20 +16,37 @@ class LocalFeedLoader {
     }
     
     func save(_ items:[FeedItem]) {
-        store.deleteCachedFeed()
+        store.deleteCachedFeed { [unowned self] error in
+            if error == nil {
+                self.store.insert(items)
+            }
+        }
     }
 }
 
 class FeedStore {
-    var deleteCachedFeedCallCount = 0
-    var insertCallCount = 0 
+    typealias DeletionCompletion = (Error?) -> Void
     
-    func deleteCachedFeed() {
+    var deleteCachedFeedCallCount = 0
+    var insertCallCount = 0
+    
+    private var deletionCompletions = [DeletionCompletion]()
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         deleteCachedFeedCallCount += 1
+        deletionCompletions.append(completion)
     }
     
     func completeDeletion(with error:Error, at index:Int = 0) {
-        
+        deletionCompletions[index](error)
+    }
+    
+    func completeDeletionSuccessfully(at index:Int = 0) {
+        deletionCompletions[index](nil)
+    }
+    
+    func insert(_ items:[FeedItem]) {
+        insertCallCount += 1
     }
 }
 
@@ -57,6 +74,15 @@ class CacheFeeduseCase: XCTestCase {
         store.completeDeletion(with: deletionError)
         
         XCTAssertEqual(store.insertCallCount, 0)
+    }
+    
+    func test_save_requestsNewCacheInsertionOnSuccesfulDeletion() {
+        let (sut, store)  = makeSUT()
+        let items = [uniqueItem(), uniqueItem()]
+        sut.save(items)
+        store.completeDeletionSuccessfully()
+        
+        XCTAssertEqual(store.insertCallCount, 1)
     }
     
     //MARK: -Helpers
