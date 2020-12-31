1843e9ed3012219efd5ab2d249e1daea2fb12c40
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-12-31 18:39:52 -0500

    Move LocalfeedItem to its own file.

---
+++ EssentialFeed2/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,24 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed2
+//
+//  Created by Ricardo Herrera Petit on 12/31/20.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id:UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id:UUID, description:String?, location:String?, imageURL:URL ) {
+        
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL  = imageURL
+        
+    }
+}
