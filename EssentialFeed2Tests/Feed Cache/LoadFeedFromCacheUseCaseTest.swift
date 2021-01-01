d1f825c1921b791184e7d3bdb939ab59413836f5
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-01-01 18:18:05 -0500

    Load command fails on retrieval error.

--- EssentialFeed2Tests/Feed Cache/LoadFeedFromCacheUseCaseTest.swift
+++ EssentialFeed2Tests/Feed Cache/LoadFeedFromCacheUseCaseTest.swift
@@ -15,12 +15,28 @@ class LoadFeedFromCacheUseCaseTest: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [])
     }
     
-    func test_load_requestsCacheRetrieval() {
+    func test_load_failsOnRetrievalError() {
         let (sut, store)  = makeSUT()
         
-        sut.load()
+        sut.load() { _ in }
         
         XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
+    func test_load_requestsCacheRetrieval() {
+        let (sut, store)  = makeSUT()
+        let exp = expectation(description: "Wait for load completion")
+        let retrievalError = anyNSError()
+        
+        var receivedError:Error?
+        sut.load { error in
+            receivedError = error
+            exp.fulfill()
+        }
+        store.completeRetrieval(with: retrievalError)
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertEqual(receivedError as NSError? , retrievalError)
     }
     
     //MARK: -Helpers
