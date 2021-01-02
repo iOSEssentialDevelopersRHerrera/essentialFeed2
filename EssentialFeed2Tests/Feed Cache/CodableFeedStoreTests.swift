cd3b3882fb2a311f35e07c9274d9da32d5d44f95
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-01-02 00:16:43 -0500

    Rertrieving form empty cache delivers empty result.

---
+++ EssentialFeed2Tests/Feed Cache/CodableFeedStoreTests.swift
@@ -0,0 +1,35 @@
+//
+//  CodableFeedStoreTests.swift
+//  EssentialFeed2Tests
+//
+//  Created by Ricardo Herrera Petit on 1/2/21.
+//
+
+import XCTest
+import EssentialFeed2
+
+class CodableFeedStore {
+    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
+        completion(.empty)
+    }
+}
+
+class CodableFeedStoreTests: XCTestCase {
+    
+    func test_retrieve_deeliversEmptyOnEmptyCache() {
+        let sut = CodableFeedStore()
+        let exp = expectation(description: "Wait for cache retrieval")
+        
+        sut.retrieve { result in
+            switch result {
+            case .empty:
+                break
+            default:
+                XCTFail("Expected empty result, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+}
