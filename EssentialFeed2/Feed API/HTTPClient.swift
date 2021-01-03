21bf1224dd18a17f014cfd2692bf0747f7229341
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-01-02 22:41:17 -0500

    committed

--- EssentialFeed2/Feed API/HTTPClient.swift
+++ EssentialFeed2/Feed API/HTTPClient.swift
@@ -13,5 +13,8 @@ public enum HTTPClientResult {
 }
 
 public protocol HTTPClient {
+    ///The Completion handler can be invoked in any thread.
+    /// Clients are responsible to appropiate threads , if needed
+    
     func get(from url:URL, completion: @escaping (HTTPClientResult) -> Void)
 }
