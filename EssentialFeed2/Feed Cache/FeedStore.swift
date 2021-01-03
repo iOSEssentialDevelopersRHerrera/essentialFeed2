b5d81756a7bd4e0370091feb9005f40d004ee081
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-01-02 22:40:59 -0500

    Side effects CodableFeedStoreTests feed store.

--- EssentialFeed2/Feed Cache/FeedStore.swift
+++ EssentialFeed2/Feed Cache/FeedStore.swift
@@ -19,8 +19,16 @@ public protocol FeedStore {
     typealias InsertionCompletion = (Error?) -> Void
     typealias RetrievalCompletion = (RetrieveCachedFeedResult) -> Void
     
+    ///The Completion handler can be invoked in any thread.
+    /// Clients are responsible to appropiate threads , if needed
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    
+    ///The Completion handler can be invoked in any thread.
+    /// Clients are responsible to appropiate threads , if needed
     func insert(_ feed:[LocalFeedImage], timestamp:Date, completion: @escaping InsertionCompletion)
+    
+    ///The Completion handler can be invoked in any thread.
+    /// Clients are responsible to appropiate threads , if needed
     func retrieve(completion: @escaping RetrievalCompletion)
 }
 
