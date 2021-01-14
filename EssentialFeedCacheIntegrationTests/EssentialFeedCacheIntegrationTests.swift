44f710f62dff78352ff0742e609bd75734765fb3
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-01-13 20:47:04 -0500

    Add EssentialFeedCache Target to separate the potentially-slow integration tests from fast and isolated tests

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Ricardo Herrera Petit on 1/13/21.
+//
+
+import XCTest
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+    
+}
