fd9aacd54eca3949d1b7ec227e33eddfe5d85f2b
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-01-20 23:26:56 -0500

    Add Presentable View Models as pure data to clarify communication between Presentation and UI

--- Essentialfeed2iOS/Feed UI/Feed Presentation/FeedPresenter.swift
+++ Essentialfeed2iOS/Feed UI/Feed Presentation/FeedPresenter.swift
@@ -8,13 +8,25 @@
 import Foundation
 import EssentialFeed2
 
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
+
 protocol FeedLoadingView: class {
-    func display(isLoading:Bool)
+    func display(_ viewModel: FeedLoadingViewModel)
+}
+
+
+struct FeedViewModel {
+    let feed: [FeedImage]
 }
 
 protocol FeedView  {
-    func display(feed: [FeedImage])
+    func display(_ viewModel:FeedViewModel)
 }
+
+
 
 final class FeedPresenter {
     typealias Observer<T> = (T) -> Void
@@ -28,12 +40,12 @@ final class FeedPresenter {
     var loadingView: FeedLoadingView?
     
     func loadFeed() {
-        loadingView?.display(isLoading: true)
+        loadingView?.display(FeedLoadingViewModel(isLoading: true))
         feedLoader.load { [weak self] result in
             if let feed = try? result.get() {
-                self?.feedview?.display(feed: feed)
+                self?.feedview?.display(FeedViewModel(feed: feed))
             }
-            self?.loadingView?.display(isLoading: false)
+            self?.loadingView?.display(FeedLoadingViewModel(isLoading: false))
         }
     }
 }
