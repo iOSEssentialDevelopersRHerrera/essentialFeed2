acced2b440fd30f8eb345983b2e85d9e97473708
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-01-30 18:24:54 -0500

    Move FeedLoafingViewModel to a separate file.

---
+++ EssentialFeed2/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed2
+//
+//  Created by Ricardo Herrera Petit on 1/30/21.
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
