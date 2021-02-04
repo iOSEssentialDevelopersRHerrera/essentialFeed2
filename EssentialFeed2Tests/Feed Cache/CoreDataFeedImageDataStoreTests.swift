eebba16aafef8b96d1e257b10400b23e2e69237b
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-02-03 23:13:42 -0500

    `CoreDataFeedStore.retrieveImageData` delivers `image data not found` when store is not empty but there's no image with matching url

--- EssentialFeed2Tests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeed2Tests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -30,6 +30,16 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
         
     }
     
+    func test_retrieveImageData_deliversNotFoundWhenStoreDataURLDoesNotMatch() {
+        let sut = makeUST()
+        let url = URL(string: "http://a-url.com")!
+        let nonMatchingURL = URL(string: "http://snother-url.com")!
+        
+        insert(anyData(), for: url, into: sut)
+        
+        expect(sut, toCompleteRetrievalwith: notFound(), for: nonMatchingURL)
+    }
+    
     //MARK: - Helpers
     
     private func makeUST(file: StaticString = #filePath, line: UInt = #line) -> CoreDataFeedStore {
@@ -55,6 +65,30 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
             }
             exp.fulfill()
         }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    private func localImage(url: URL) -> LocalFeedImage {
+        return LocalFeedImage(id: UUID(), description: "any", location: "any", url: url)
+    }
+    
+    private func insert(_ data: Data, for url:URL, into sut: CoreDataFeedStore, file: StaticString = #filePath, line: UInt = #line ) {
+        let exp = expectation(description: "Wait for cache insertion")
+        let image = localImage(url: url)
+        sut.insert([image], timestamp: Date()) { result in
+            switch result {
+            case let .failure(error):
+                XCTFail("Failed to save \(image) with error \(error)", file: file, line: line)
+            case .success:
+                sut.insert(data, for: url) { result in
+                    if case let Result.failure(error) = result {
+                        XCTFail("Failed to insert \(data) with error \(error)", file: file, line: line)
+                    }
+                }
+            }
+            exp.fulfill()
+        }
+        
         wait(for: [exp], timeout: 1.0)
     }
 }
