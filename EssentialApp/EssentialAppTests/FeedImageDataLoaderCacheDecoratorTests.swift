140da699dc7a41f4b179e857ac84ebcb806f3e76
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-03-03 21:07:23 -0500

    Move `FeedImageDataCache` to the `FeedFeature` production group.

--- EssentialApp/EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
@@ -9,12 +9,6 @@ import XCTest
 import EssentialFeed2
 import EssentialApp
 
-protocol FeedImageDataCache {
-    typealias Result = Swift.Result<Void,Error>
-    
-    func save(_ data: Data, for url:URL, completion: @escaping (Result) -> Void)
-}
-
 class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
     private let decoratee: FeedImageDataLoader
     private let cache: FeedImageDataCache
@@ -28,7 +22,7 @@ class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
         return decoratee.loadImageData(from: url) { [weak self] result in
             completion(result.map { data in
                 self?.cache.save(data, for: url) { _ in }
-                return data 
+                return data
             })
         }
     }
