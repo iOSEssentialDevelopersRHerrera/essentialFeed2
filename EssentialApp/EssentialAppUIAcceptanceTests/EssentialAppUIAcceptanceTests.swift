038703aa61bb65769c56c3af702272fbda616f9e
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-03-08 20:36:52 -0500

    Reset cache in every test run to make sure the tests run in a clean state and don't influence the result of other tests.

--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -12,7 +12,7 @@ class EssentialAppUIAcceptanceTests: XCTestCase {
     func test_onLaunch_displayRemoteFeedWhenCustomerHasConnectivity() {
         
         let app = XCUIApplication()
-        
+        app.launchArguments = ["-reset"]
         app.launch()
         
         
@@ -24,19 +24,20 @@ class EssentialAppUIAcceptanceTests: XCTestCase {
     }
     
     func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
-             let onlineApp = XCUIApplication()
-             onlineApp.launch()
-
-             let offlineApp = XCUIApplication()
-             offlineApp.launchArguments = ["-connectivity", "offline"]
-             offlineApp.launch()
-
-             let cachedFeedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
-             XCTAssertEqual(cachedFeedCells.count, 22)
-
-             let firstCachedImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
-             XCTAssertTrue(firstCachedImage.exists)
-         }
+        let onlineApp = XCUIApplication()
+        onlineApp.launchArguments = ["-reset"]
+        onlineApp.launch()
+        
+        let offlineApp = XCUIApplication()
+        offlineApp.launchArguments = ["-connectivity", "offline"]
+        offlineApp.launch()
+        
+        let cachedFeedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
+        XCTAssertEqual(cachedFeedCells.count, 22)
+        
+        let firstCachedImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
+        XCTAssertTrue(firstCachedImage.exists)
+    }
     
     func test_onLaunch_displaysEmptyFeedWhenCustomerHasNoConnectivityAndNoCache() {
         let app = XCUIApplication()
