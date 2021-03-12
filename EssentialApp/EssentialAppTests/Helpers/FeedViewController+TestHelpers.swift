35e95cd2f3dbd002ad45029840cf8141e0546457
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-03-11 20:43:43 -0500

    Displays remote feed on launch when customer has connectivity

--- EssentialApp/EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
@@ -43,6 +43,11 @@ extension FeedViewController {
         ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
     }
     
+    func renderedFeedImageData(at index: Int) -> Data? {
+        return simulateFeedImageViewVisible(at: index)?.renderedImage
+    }
+    
+    
     var errorMessage: String? {
         return errorView?.message
     }
